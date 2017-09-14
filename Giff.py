0072206fad5daf6b5cc36971ffdcfe837af88aeb
dhantha <dhanthagunarathna@yahoo.com>
2017-09-13 21:35:44 -0400

    able to upload and call python

---
+++ Giff.py
@@ -0,0 +1,18 @@
+import imageio
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+
+
+path = './' + sys.argv[1]
+name = sys.argv[2]
+files = os.listdir(path)
+
+imagefiles = []
+for file in files:
+	imagefiles.append(imageio.imread(path + '/' + file))
+
+
+imageio.mimsave(path + '/' + name + '.gif', imagefiles)
--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+//var multer = require('multer');
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('file-system');
+
+var app = express()
 
 app.use(express.static('./'))
 
-app.get('/', function (req, res) {
-  
-})
+app.post('/api/photo', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.multiples = true;
+    
+    // need to create the dir dynamically
+    var pathToFolder = new Date().toISOString()
+    
+    fs.mkdirSync(pathToFolder,0777);
+    
+    form.uploadDir = path.join(__dirname, '/' + pathToFolder);
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+    });
+    
+    // create the giff
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',["./Giff.py", pathToFolder, pathToFolder]);
+                                  
+                                  
+    form.on('end', function() {
+        res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
