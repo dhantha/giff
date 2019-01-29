2fdda60d3592976af477e26537e5655c8b5a4966
dhantha <dhanthagunarathna@yahoo.com>
2019-01-29 04:33:45 +0000

    giff imager v1.2

--- client/public/client.js
+++ client/public/client.js
@@ -3,7 +3,7 @@ $(document).ready(function($){
   $('#progress').hide();
 
   var delay = 50;
-  var width =  $('#progressBar')[0].style.width;
+  var width = $('#progressBar')[0].style.width;
   $('.dial').knob({
         'release' : function (v) {
           delay = v;
@@ -13,20 +13,24 @@ $(document).ready(function($){
   $('#uploadForm').on('submit', function(e){
     e.preventDefault();
     $('#progress').show();
+
     frame();
     //get the input files
     var input = $('#uploadPhotos')[0].files
-    console.log(input);
     // start display the progress bar
     getValidFileList(input, onDone);
   });
 
   function frame(){
     var progressBar = $('#progressBar')[0];
-    console.log(progressBar);
     for(var i = 0; i < 25; i++){
       width++;
       progressBar.style.width = width + '%'
+      // if done loading set the width to 0
+      if(width == 100){
+         //$('#progress').hide();
+         width = 0;
+      }
     }
   }
 
@@ -34,7 +38,6 @@ $(document).ready(function($){
     // get the 1st image width and height
     // end display the progress bar
     frame();
-    console.log(delay);
     var img = fileList[0]
     var imgHeight = img.height;
     var imgWidth = img.width;
